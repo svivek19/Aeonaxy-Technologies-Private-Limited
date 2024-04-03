b5e13310d33416d05672b0fb3a60d090144faa3e
svivek19 <svivek030503@gmail.com>
2024-04-03 12:05:02 +0530

    second page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FirstPage from "./pages/FirstPage";
+import SecondPage from "./pages/SecondPage";
 
 const App = () => {
   return (
     <>
-      <FirstPage />
+      {/* <FirstPage /> */}
+      <SecondPage />
     </>
   );
 };
